ede8fe2407a496e1eb40babc200dfe4d876a402e
3mmvnu3l <emm.moreno@duocuc.cl>
2025-08-20 12:10:14 -0400

    first commit

---
+++ game.py
@@ -0,0 +1,23 @@
+import pygame;
+
+class Game:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+    
+    def new(self):
+        pass
+
+    def run(self):
+        pass
+    def events(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+    def stop(self):
+        pass
+
