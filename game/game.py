c56b4d97fa549cdf5c0d78a71eaf8f78b787cb63
3mmvnu3l <emm.moreno@duocuc.cl>
2025-08-20 22:57:44 -0400

    clase17. creacion de monedas

--- game/game.py
+++ game/game.py
@@ -7,6 +7,7 @@ from .colors import* ## importación de colores
 from .platform import Platform
 from .player import Player
 from .wall import Wall
+from .coin import Coin
 
 class Game:
     def __init__(self):
@@ -30,9 +31,12 @@ class Game:
 
         self.sprites = pygame.sprite.Group()
         self.walls = pygame.sprite.Group()
+        self.coins = pygame.sprite.Group()
+
         self.sprites.add(self.platform)
         self.sprites.add(self.player)
         self.generate_walls()
+        self.generate_coins()
     
     def generate_walls(self):
         last_position = WIDTH + 100 ##variable que otorga rango entre un obstaculo y otro
@@ -44,6 +48,18 @@ class Game:
 
                 self.sprites.add(wall)
                 self.walls.add(wall)
+                
+
+    def generate_coins(self):
+        last_position = WIDTH + 100
+        for c in range (0, MAX_COINS):
+            pos_x = random.randrange(last_position + 180, last_position + 300)
+            coin = Coin(pos_x, 150)
+            last_position = coin.rect.right
+
+            self.sprites.add(coin)
+            self.coins.add(coin)
+
 
     def run(self):
         while self.running:
